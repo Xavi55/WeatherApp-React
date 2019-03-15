1351029d86e812a90ffd7fda5075a7dbd815cd56
Xavi55 <evinxavier5@gmail.com>
2019-03-15 01:01:07 -0400

    Updates

---
+++ precache-manifest.56b12017c257a4ef1d00e10038fd3c1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ff246e946d46ed7eb05",
+    "url": "/ReactWeatherApp/static/js/runtime~main.8ff246e9.js"
+  },
+  {
+    "revision": "8dd0a9e81eb01db2387f",
+    "url": "/ReactWeatherApp/static/js/main.8dd0a9e8.chunk.js"
+  },
+  {
+    "revision": "9fa6db3cf9a791021e5b",
+    "url": "/ReactWeatherApp/static/js/1.9fa6db3c.chunk.js"
+  },
+  {
+    "revision": "8dd0a9e81eb01db2387f",
+    "url": "/ReactWeatherApp/static/css/main.d6ca1e48.chunk.css"
+  },
+  {
+    "revision": "9cfe6a311e64adcb4c3f7e623e683a83",
+    "url": "/ReactWeatherApp/index.html"
+  }
+];
