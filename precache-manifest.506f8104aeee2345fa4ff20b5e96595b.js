3b5ef65cb6b5c6076e5f23fd97b26f99daae43bd
Xavi55 <evinxavier5@gmail.com>
2019-03-16 19:58:47 -0400

    Updates

---
+++ precache-manifest.506f8104aeee2345fa4ff20b5e96595b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07480de44ad0e2f53670",
+    "url": "/WeatherApp-React/static/js/runtime~main.07480de4.js"
+  },
+  {
+    "revision": "18e7f103d4b94386e557",
+    "url": "/WeatherApp-React/static/js/main.18e7f103.chunk.js"
+  },
+  {
+    "revision": "47c9e62c8ee8274b667f",
+    "url": "/WeatherApp-React/static/js/1.47c9e62c.chunk.js"
+  },
+  {
+    "revision": "18e7f103d4b94386e557",
+    "url": "/WeatherApp-React/static/css/main.bc46949e.chunk.css"
+  },
+  {
+    "revision": "589540718fe375986ffa36afe46a2ad3",
+    "url": "/WeatherApp-React/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/WeatherApp-React/precache-manifest.8ece631782508d7d02b3874a468a0e9e.js"
+  "/WeatherApp-React/precache-manifest.506f8104aeee2345fa4ff20b5e96595b.js"
 );
 
 workbox.clientsClaim();
